c141efed49e227ab163e35d41688af0aefa0af67
yassirelkhaili <elkhailiyassir@gmail.com>
2023-09-14 12:16:48 +0100

    co

--- C2/ex08/ex08.c
+++ C2/ex08/ex08.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
+void toOctal(int octal)
+{
+    if(octal != 0)
+        toOctal(octal / 8);
+    if (octal % 8 != 0)
+        printf("%d", (octal % 8));
+}
+
+void toHexa(int hexa)
+{
+    char hexadecimal[] = "0123456789ABCDEF";
+    if(hexa != 0)
+        toHexa(hexa / 16);
+    if (hexa % 16 != 0)
+        printf("%c", hexadecimal[hexa % 16]);
+}
+
 int main ()
 {
     int num;
-    printf("Enter the number you wish to convert: \n");
+    char *result;
+    printf("Enter a number: \n");
     scanf("%d", &num);
-    printf("Your number in Octal: %o\n", num);
-    printf("Your number in Hexadecimal: %X\n", num);
+    printf("Number in octal: ");
+    toOctal(num);
+    printf("\n");
+    printf("Number in hexadecimal: ");
+    toHexa(num);
     return 0;
 }
