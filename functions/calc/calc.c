4a043a5644313090b30a64cf18628630c1e94858
yassirelkhaili <elkhailiyassir@gmail.com>
2023-09-16 16:31:26 +0100

    program still has bugs

--- functions/calc/calc.c
+++ functions/calc/calc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int ft_div(int a, int b)
 {
@@ -50,18 +51,90 @@ void get_user_choice(int *user_choice)
     printf("5. Division\n");
     printf("6. Modulus (remainder)\n");
     printf("7. Square Root\n");
-    printf("8. Exit\n");
+    printf("8. Arrays\n");
+    printf("9. Exit\n");
     scanf("%d", &choice);
     *user_choice = choice;
+}
+
+void arr_menu(int *arr_choice)
+{
+     int choice;
+    printf("Choose an operation:\n");
+    printf("1. min\n");
+    printf("2. max\n");
+    printf("3. average\n");
+    printf("4. Exit\n");
+    scanf("%d", &choice);
+    *arr_choice = choice;
+}
+
+int ft_malloc(char **name)
+{
+    int length = 0;
+    char ch;
+    *name = NULL;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+    {
+        *name = (char *)realloc(*name, (length + 1) * sizeof(char));
+        if (*name == NULL)
+        {
+            printf("Memory allocation failure");
+            return 1;
+        }
+        (*name)[length] = ch;
+        length++;
+    }
+    *name = (char *)realloc(*name, (length + 1) * sizeof(char));
+    if (*name == NULL)
+        {
+            printf("Memory allocation failure");
+            return 1;
+        }
+    (*name)[length] = '\0';
+    return 0; 
+}
+
+int ft_strlen(char *str)
+{
+    int strlen = 0;
+    while (*str)
+    {
+        strlen++;
+        str++;
+    }
+    return strlen;
+}
+
+void bubblesort(int *arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main ()
 {
     int user_choice;
+    int arr_choice;
     get_user_choice(&user_choice);
     do {
         int num1;
         int num2;
+        char *string = NULL;
+        int *nums = NULL;
+        int size = 0;
+        int index = 0;
+        int strlen;
         switch (user_choice)
         {
             case 1:
@@ -125,11 +198,118 @@ int main ()
             printf("----End of Operation----\n");
             get_user_choice(&user_choice);
             break;
+            case 8:
+            arr_menu(&arr_choice);
+            do
+            {
+            switch(arr_choice)
+            {
+            case 1:
+            printf("Enter a string of numbers:\n");
+            getchar();
+            if (ft_malloc(&string) == 0) {
+            int strlen = ft_strlen(string);
+            nums = (int *)malloc(strlen * sizeof(int));
+            while (*string) {
+            int number = 0;
+            while(*string >= 48 && *string <= 57)
+            {
+                number = number * 10 + (*string - 48);
+                string++;
+            }
+            nums[index] = number;
+            index++;
+            if (*string)
+            string++;
+            }
+            size = sizeof(nums) / sizeof(nums[0]);
+            bubblesort(nums, size);
+            printf("The smallest number is: %d\n", nums[0]);
+            free(string);
+            free(nums);
+             printf("----End of Operation----\n");
+             arr_menu(&arr_choice);
+            } else {
+            printf("An error has occurred. Please check your input and try again.\n");
+            return 1;
+            }
+            break;
+            case 2:
+            printf("Enter a string of numbers:\n");
+            getchar();
+            if (ft_malloc(&string) == 0) {
+            int strlen = ft_strlen(string);
+            nums = (int *)malloc(strlen * sizeof(int));
+           while (*string) {
+            int number = 0;
+            while(*string >= 48 && *string <= 57)
+            {
+                number = number * 10 + (*string - 48);
+                string++;
+            }
+            nums[index] = number;
+            index++;
+            if (*string)
+            string++;
+            }
+            size = sizeof(nums) / sizeof(nums[0]);
+            bubblesort(nums, size);
+            printf("%d", nums[3]);
+            printf("The biggest number is: %d\n", nums[size - 1]);
+            free(string);
+            free(nums);
+             printf("----End of Operation----\n");
+             arr_menu(&arr_choice);
+            } else {
+            printf("An error has occurred. Please check your input and try again.\n");
+            return 1;
+            }
+            printf("----End of Operation----\n");
+            arr_menu(&arr_choice);
+            break;
+            case 3:
+            printf("Enter a string of numbers:\n");
+            getchar();
+            if (ft_malloc(&string) == 0) {
+            int strlen = ft_strlen(string);
+            nums = (int *)malloc(strlen * sizeof(int));
+            while (*string) {
+            int number = 0;
+            while(*string >= 48 && *string <= 57)
+            {
+                number = number * 10 + (*string - 48);
+                string++;
+            }
+            nums[index] = number;
+            index++;
+            if (*string)
+            string++;
+            }
+            size = sizeof(nums) / sizeof(nums[0]);
+            bubblesort(nums, size);
+            printf("The average number is: %d\n", nums[(size - 1) / 2]);
+            free(string);
+            free(nums);
+             printf("----End of Operation----\n");
+             arr_menu(&arr_choice);
+            } else {
+            printf("An error has occurred. Please check your input and try again.\n");
+            return 1;
+            }
+            printf("----End of Operation----\n");
+            arr_menu(&arr_choice);
+            break;
+            default:
+            printf("invalid choice\n");
+            break;
+            }
+            } while (arr_choice != 4);
+            break;
             default:
             printf("Invalid choice\n");
             break;
         }
-    } while(user_choice != 8);
+    } while(user_choice != 9);
     printf("----End of Program----");
     return 0;
 }
